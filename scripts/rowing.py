e6d9d2143f723fff609b706d1a120a9f633a8004
Yuya Kono <takohati0821@gmail.com>
2024-02-05 20:01:45 +0900

    Refactor code to use classes for gravity filtering and history tracking

--- scripts/rowing.py
+++ scripts/rowing.py
@@ -36,35 +36,71 @@ def getAccel(joycon: JoyCon) -> Dict[str, float]:
 def addAverage(left: Dict[str, float], right: Dict[str, float]) -> Dict[str, float]:
     return {"x": (left["x"] + right["x"]) / 2, "y": (left["y"] + right["y"]) / 2, "z": (left["z"] + right["z"]) / 2}
 
-def appendAccel(accel: Dict[str, float], X: Deque[float], Y: Deque[float], Z: Deque[float]) -> None:
-    X.append(np.abs(accel["x"]))
-    Y.append(np.abs(accel["y"]))
-    Z.append(np.abs(accel["z"]))
+# def appendAccel(accel: Dict[str, float], X: Deque[float], Y: Deque[float], Z: Deque[float]) -> None:
+#     X.append(np.abs(accel["x"]))
+#     Y.append(np.abs(accel["y"]))
+#     Z.append(np.abs(accel["z"]))
 
-gravity: List[float] = [0, 0, 0]
-def filterGravity(accel: Dict[str, float]) -> Dict[str, float]:
-    global gravity
-    alpha = 0.8
-    gravity[0] = alpha * gravity[0] + (1 - alpha) * accel["x"]
-    gravity[1] = alpha * gravity[1] + (1 - alpha) * accel["y"]
-    gravity[2] = alpha * gravity[2] + (1 - alpha) * accel["z"]
+class GravityFilter:
+    alpha: float
+    gravity: List[float]
 
-    return {"x": accel["x"] - gravity[0], "y": accel["y"] - gravity[1], "z": accel["z"] - gravity[2]}
+    def __init__(self, alpha: float) -> None:
+        self.alpha = alpha
+        self.gravity = [0, 0, 0]
+    
+    def filter(self, accel: Dict[str, float]) -> Dict[str, float]:
+        self.gravity[0] = self.alpha * self.gravity[0] + (1 - self.alpha) * accel["x"]
+        self.gravity[1] = self.alpha * self.gravity[1] + (1 - self.alpha) * accel["y"]
+        self.gravity[2] = self.alpha * self.gravity[2] + (1 - self.alpha) * accel["z"]
+        return {"x": accel["x"] - self.gravity[0], "y": accel["y"] - self.gravity[1], "z": accel["z"] - self.gravity[2]}
 
-# 加速度の積分の総和
-def getVelocity(X: Deque[float], Y: Deque[float], Z: Deque[float]) -> float:
-    vx = np.trapz(X) / len(X)
-    vy = np.trapz(Y) / len(Y)
-    vz = np.trapz(Z) / len(Z)
-    return vx + vy + vz
+# gravity: List[float] = [0, 0, 0]
+# def filterGravity(accel: Dict[str, float]) -> Dict[str, float]:
+#     global gravity
+#     alpha = 0.8
+#     gravity[0] = alpha * gravity[0] + (1 - alpha) * accel["x"]
+#     gravity[1] = alpha * gravity[1] + (1 - alpha) * accel["y"]
+#     gravity[2] = alpha * gravity[2] + (1 - alpha) * accel["z"]
+# 
+#     return {"x": accel["x"] - gravity[0], "y": accel["y"] - gravity[1], "z": accel["z"] - gravity[2]}
+
+class History:
+    X: Deque[float]
+    Y: Deque[float]
+    Z: Deque[float]
+    
+    def __init__(self, length: int) -> None:
+        self.X = Deque(maxlen=length)
+        self.Y = Deque(maxlen=length)
+        self.Z = Deque(maxlen=length)
+    
+    def append(self, accel: Dict[str, float]) -> None:
+        self.X.append(np.abs(accel["x"]))
+        self.Y.append(np.abs(accel["y"]))
+        self.Z.append(np.abs(accel["z"]))
+    
+    # 加速度の積分の総和
+    def velocity(self) -> float:
+        vx = np.trapz(self.X) / len(self.X)
+        vy = np.trapz(self.Y) / len(self.Y)
+        vz = np.trapz(self.Z) / len(self.Z)
+        return vx + vy + vz
+
+# def getVelocity(X: Deque[float], Y: Deque[float], Z: Deque[float]) -> float:
+#     vx = np.trapz(X) / len(X)
+#     vy = np.trapz(Y) / len(Y)
+#     vz = np.trapz(Z) / len(Z)
+#     return vx + vy + vz
 
 def main():
     dataLength = 60
     threshold = 700
 
-    X = Deque(maxlen=dataLength)
-    Y = Deque(maxlen=dataLength)
-    Z = Deque(maxlen=dataLength)
+    # X = Deque(maxlen=dataLength)
+    # Y = Deque(maxlen=dataLength)
+    # Z = Deque(maxlen=dataLength)
+    avgHistory = History(dataLength)
 
     # (ljoycon, rjoycon) = setupJoyCon()
     lid = device.get_L_id()
@@ -77,6 +113,8 @@ def main():
 
     lAcc = 0
     rAcc = 0
+    
+    avgFilter = GravityFilter(0.8)
 
     while True:
         # watch for connection
@@ -116,14 +154,16 @@ def main():
         if not rjoycon is None:
             rAcc = getAccel(rjoycon)
 
-        accel = addAverage(lAcc, rAcc)
-        accel = filterGravity(accel)
-        logging.info(f"accel: {accel}")
-        appendAccel(accel, X, Y, Z)
-        velocity = getVelocity(X, Y, Z)
-        logging.info(f"velocity: {velocity}")
+        avgAccel = addAverage(lAcc, rAcc)
+        avgAccel = avgFilter.filter(avgAccel)
+        logging.info(f"accel: {avgAccel}")
+        avgHistory.append(avgAccel)
+        # appendAccel(accel, X, Y, Z)
+        avgVelocity = avgHistory.velocity()
+        # velocity = getVelocity(X, Y, Z)
+        logging.info(f"velocity: {avgVelocity}")
 
-        if velocity > threshold and not is_pressed:
+        if avgVelocity > threshold and not is_pressed:
             is_pressed = True
             logging.info("pressed")
             pyautogui.keyDown('s')
@@ -132,7 +172,7 @@ def main():
                 urllib.request.urlopen(serverAddress + "/start").read()
             except Exception as e:
                 logging.error(e)
-        elif velocity <= threshold and is_pressed:
+        elif avgVelocity <= threshold and is_pressed:
             is_pressed = False
             logging.info("released")
             pyautogui.keyUp('s')
